5d269d7f120f6ef190e707a7973372a910171206
Minhyeok Kim <basakgrape@gmail.com>
2026-09-10 00:23:59 +0900

    load lyric readability fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./learning-enhancements.css";
+import "./readability-fixes.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
